623948e772986adc87b17525fa300b059141b32b
Rodolfo P Araujo <rodolfop.araujo@gmail.com>
2022-11-25 11:09:44 -0300

    Add files via upload
    Init program for this class

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FimTPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 10;
+            int h = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < h; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                h++;
+                y--;
+                Console.ReadKey();
+
+            }
+            
+        }
+    }
+}
